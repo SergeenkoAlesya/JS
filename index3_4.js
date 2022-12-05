fd11e243a80c1767afafc5be7bf9ebf683840ac2
SergeenkoAlesya <sergeenkoAlesya@gmail.com>
2022-12-05 15:42:17 +0300

    fixed index3_4.js

--- index3_4.js
+++ index3_4.js
@@ -1,5 +1,6 @@
 /*4.	 Функция принимает строку и проверяет её на палиндром. В случае если строка является палиндромом возвращает true иначе false*/
 
-const isPalindrome = (word) => word === word.split("").reverse().join("");
-console.log("№4 ", isPalindrome("Привет"));
-console.log("№4 ", isPalindrome("шалаш"));
+const isPalindrome = (word) =>
+  word.toLowerCase() === word.split("").reverse().join("").toLowerCase();
+console.log("№4 ", isPalindrome("ПривеТ"));
+console.log("№4 ", isPalindrome("шалаШ"));
