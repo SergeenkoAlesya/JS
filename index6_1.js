d6cb22119d129598527b0f7a85591848d1e4503d
SergeenkoAlesya <sergeenkoAlesya@gmail.com>
2023-01-10 18:32:17 +0300

    update index6_1

--- index6_1.js
+++ index6_1.js
@@ -5,10 +5,14 @@ const moveToZero = (date) => {
     const element = date[index];
     //console.log(element, index, countZero);
     if (element === 0 || element === "0") {
-      date.push(date[index]);
-
-      date.splice(index, 1);
+      let index2 = index;
+      for (index2; index2 < maxIndex - 1; index2++) {
+        //console.log(date[index2], date[index2 + 1]);
+        date[index2] = date[index2 + 1];
+      }
+      date[index2] = element;
       //console.log(date);
+      //console.log(element);
       index--;
       countZero++;
     }
@@ -17,4 +21,7 @@ const moveToZero = (date) => {
   return date;
 };
 
-console.log(moveToZero([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]));
+console.log(
+  "6.1 ",
+  moveToZero([7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14])
+);
